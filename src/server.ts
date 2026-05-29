74719efef1babbf9b448003c6f71cbb95cd79168
Dhawanpreet kaur <dkaur36@academic.rrc.ca>
2026-05-29 00:28:36 -0500

    feat: connect portfolio function to Express API endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,31 @@
-import app from "./app";
+import express from "express";
+import { calculatePortfolioPerformance } from "./portfolio/portfolioPerformance";
+
+const app = express();
+
+app.use(express.json());
+
+//health check endpoint
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "ok"
+    });
+});
+
+//Portfolio endpoint
+app.post("/portfolio/performance", (req, res) => {
+    const { initialInvestment, currentValue } = req.body;
+
+    const result = calculatePortfolioPerformance(
+        initialInvestment,
+        currentValue
+    );
+
+    res.status(200).json(result);
+});
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
